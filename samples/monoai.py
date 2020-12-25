b37657b72104f4a0aca696519dcf8922701b9cf7
neumond <knifeslaughter@gmail.com>
2020-12-25 15:44:22 +0300

    Better monoai script

--- samples/monoai.py
+++ samples/monoai.py
@@ -1,50 +1,60 @@
 from void import (
     M,
-    switch1, vault1,
+    message1, sorter1, vault1, container1,
+    last_ore,
 )
 
+container = vault1 or container1
+if not container:
+    M.print('No linked container')
+    message1.printFlush()
+    M.exit()
+core_x = container.x
+core_y = container.y
 
-container = vault1
-
+if not sorter1.config:
+    M.print('Choose ore')
+    message1.printFlush()
+    M.exit()
 
 M.unit.bind(M.at.mega)
 
-
-if switch1.enabled:
-    M.unit.setFlag(0)
-
-    core_x = container.x
-    core_y = container.y
-
-    ore_x = 56
-    ore_y = 140
-
-    moving_home = 20
-    at_home = 21
-    moving_ore = 22
-    at_ore = 23
-
+if last_ore != sorter1.config:
+    if M.unit.within(core_x, core_y, 5):
+        found, ore_x, ore_y = M.locate.ore(sorter1.config)
+        if found:
+            last_ore = sorter1.config
+        else:
+            M.print('Can\'t locate ore')
+            message1.printFlush()
+        M.exit()
+    else:
+        M.print('Locating ore...')
+        message1.printFlush()
+        M.unit.approach(core_x, core_y, 5)
     M.exit()
 
+M.print('Gathering...')
+message1.printFlush()
 
-if M.unit.flag == moving_home:
+dst_core = M.dst(M.unit.x - core_x, M.unit.y - core_y)
+dst_ore = M.dst(M.unit.x - ore_x, M.unit.y - ore_y)
+
+closer_to_core = dst_core < dst_ore
+
+items = M.unit.totalItems
+cap = M.unit.itemCapacity
+
+full = items == cap
+some = 0 < items < cap
+
+if full or (some and closer_to_core):
     if M.unit.within(core_x, core_y, 5):
-        M.unit.setFlag(at_home)
+        M.unit.itemDrop(container, M.unit.totalItems)
     else:
         M.unit.approach(core_x, core_y, 5)
-elif M.unit.flag == at_home:
-    if M.unit.totalItems > 0:
-        M.unit.itemDrop(container, M.unit.totalItems)
-    M.unit.setFlag(moving_ore)
-elif M.unit.flag == moving_ore:
+else:
     if M.unit.within(ore_x, ore_y, 5):
-        M.unit.setFlag(at_ore)
+        M.unit.mine(ore_x, ore_y)
     else:
         M.unit.approach(ore_x, ore_y, 5)
-elif M.unit.flag == at_ore:
-    if M.unit.totalItems >= M.unit.itemCapacity:
-        M.unit.setFlag(moving_home)
-    else:
-        M.unit.mine(ore_x, ore_y)
-else:
-    M.unit.setFlag(moving_home)
